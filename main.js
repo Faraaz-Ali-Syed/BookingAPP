46384a2fbd7f4e3e4f70dba852191116dfd2d555
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-03-15 17:10:20 +0530

    Pushed2

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ function myfunc(e) {
    
     const full_name= document.getElementById('name').value;
     const email = document.getElementById('email').value;
-     localStorage.getItem('name', full_name);
+     
+    const obj = {
+        full_name,
+        email
+    }
+    localStorage.setItem("userdetails", JSON.stringify(obj));
+    
+    localStorage.getItem('name', full_name);
      localStorage.getItem('mail', email);
  
  } 
