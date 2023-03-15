936cef949e79590307f41c9b2802a13fe13c16e2
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-03-15 16:52:17 +0530

    Pushed1

---
+++ main.js
@@ -0,0 +1,10 @@
+function myfunc(e) {
+   
+    const full_name= document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+     localStorage.getItem('name', full_name);
+     localStorage.getItem('mail', email);
+ 
+ }
+ 
+ 
