ba76f6b6c7bd56ca2f7591a267c64c3a3511f7f3
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-03-20 17:44:01 +0530

    pushed delete btn

--- main.js
+++ main.js
@@ -20,7 +20,25 @@ function myfunc(event) {
 function showuseronscreen(obj) {
     const parentele = document.getElementById("listofitems")
 
-    parentele.innerHTML = parentele.innerHTML + '<li>${obj.name} - ${obj.email}</li>';
+    const childele = document.createElement('li')
+    childele.textContent = obj.name + '-' + obj.email;
+
+    //parentele.innerHTML = parentele.innerHTML + '<li>${obj.name} - ${obj.email}</li>';
+
+    const deletebtn = document.createElement('input');
+    deletebtn.type = "button";
+    deletebtn.value = 'Delete';
+    deletebtn.onclick = () => {
+
+        localStorage.removeItem(obj.email)
+        parentele.removeChild(childele)
+
+    }
+    childele.appendChild(deletebtn)
+    parentele.appendChild(childele)
+
 }
+
+
 
  
